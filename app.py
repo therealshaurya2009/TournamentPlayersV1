2913059dd2a90917ae7b70ba1e562f1be38d4ec1
therealshaurya2009 <135183763+therealshaurya2009@users.noreply.github.com>
2025-09-29 20:11:32 -0500

    Update app.py

--- app.py
+++ app.py
@@ -57,6 +57,13 @@ async def setup_browser():
         ),
     )
 
+    context = await browser.new_context(
+        viewport={"width": 1920, "height": 1080},
+        java_script_enabled=True,
+        user_agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36",
+    )
+
+    
     page = await context.new_page()
 
     # --- Hide headless indicators for stealth ---
