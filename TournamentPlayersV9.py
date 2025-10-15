51bb85da71aa9d94510b352f297a05d7aa173dd0
therealshaurya2009 <135183763+therealshaurya2009@users.noreply.github.com>
2025-10-15 14:28:54 -0500

    Update TournamentPlayersV9.py

--- TournamentPlayersV9.py
+++ TournamentPlayersV9.py
@@ -247,14 +247,13 @@ async def scrape_usta(player_link, age_group, max_retries: int = 5):
     while retries < max_retries:
         retries += 1
         playwright, browser, context, page = await setup_browser()
-        await page.goto(player_link + "&tab=about", wait_until="domcontentloaded", timeout=60000)
-        st.write("Player Profile Opened")
+        await page.goto(player_link + "&tab=about", wait_until="networkidle")
+        await page.wait_for_timeout(5000)  # wait 3 seconds
         player_name_selector = "span.readonly-text__text > h3"
-        await page.wait_for_selector(player_name_selector, state="attached",  timeout=60000)
+        await page.wait_for_selector(player_name_selector, state="visible", wait_until="domcontentloaded",  timeout=60000)
         locator = page.locator(player_name_selector)
         player_name = await locator.text_content()
         player_name = player_name.strip()
-        st.write(player_name)
         
         try:
             await page.goto(player_link + "&tab=about", wait_until="networkidle")
