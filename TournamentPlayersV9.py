ed73f88f65686edf015d974f4d5bd29e8dcb69b8
therealshaurya2009 <135183763+therealshaurya2009@users.noreply.github.com>
2025-10-15 13:02:26 -0500

    Update TournamentPlayersV9.py

--- TournamentPlayersV9.py
+++ TournamentPlayersV9.py
@@ -247,6 +247,9 @@ async def scrape_usta(player_link, age_group, max_retries: int = 5):
     while retries < max_retries:
         retries += 1
         playwright, browser, context, page = await setup_browser()
+        await page.goto(player_link + "&tab=about", wait_until="networkidle")
+        html = await page.content()
+        st.write(html)
 
         try:
             await page.goto(player_link + "&tab=about", wait_until="networkidle")
@@ -403,6 +406,19 @@ async def scrape_draw_size(link, selected_age_group):
 
 
 async def scrape_player(player_link, age_group):
+    player_info = await scrape_usta(player_link, age_group)
+    return {
+        "Name": player_info[0],
+        "Profile": player_link,
+        "Location": player_info[1],
+        "District": player_info[2],
+        "WTN": player_info[3],
+        "Points": player_info[4],
+        "Ranking": player_info[5],
+        "Recruiting": player_info[6],
+        "Class": player_info[8],
+        "UTR": player_info[7],
+    }
     try:
         player_info = await scrape_usta(player_link, age_group)
         return {
